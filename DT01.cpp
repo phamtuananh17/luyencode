025974160006504fc6505a9c5f70c031c193d15e
phamtuananh17 <taikhoanphucuatoi17@gmail.com>
2022-12-27 10:30:20 +0700

    tinh dien tich nua hinh tron

---
+++ DT01.cpp
@@ -0,0 +1,15 @@
+// create by  ---pham tuan anh
+//   27/12/2022
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << setprecision(3) << fixed << (n * n * M_PI_2);
+
+    return 0;
+}
